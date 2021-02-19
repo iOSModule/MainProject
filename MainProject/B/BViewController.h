ca56c3cf1234bdd994ba5dabb6d2942f81ee39c6
于鸿鹏 <hongpeng03@gmail.com>
2021-02-19 14:40:11 +0800

    feat：主工程中组件化A业务

---
+++ MainProject/B/BViewController.h
@@ -0,0 +1,16 @@
+//
+//  BViewController.h
+//  MainProject
+//
+//  Created by 于鸿鹏 on 2021/2/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MainProject/B/BViewController.m
@@ -0,0 +1,33 @@
+//
+//  BViewController.m
+//  MainProject
+//
+//  Created by 于鸿鹏 on 2021/2/5.
+//
+
+#import "BViewController.h"
+
+@interface BViewController ()
+
+@end
+
+@implementation BViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"B";
+    self.view.backgroundColor = [UIColor cyanColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
